b7f10fc9792511a512ce46efe618254eaf1f094f
Kang <ysh5022@naver.com>
2016-06-27 14:12:20 +0900

    Kang Dong Hun - Test3

---
+++ 02.Scripts/csTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csTest : MonoBehaviour
+{
+    public GameObject clone;
+    public GameObject parent;
+
+    int nName = 0;
+    Transform[] SpawnObject;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 80, 50), "Add One"))
+        {
+            GameObject goTemp = Instantiate(clone, Vector3.zero, Quaternion.identity) as GameObject;
+            goTemp.transform.parent = parent.transform;
+            goTemp.name = "child_" + nName;
+            nName++;
+        }
+
+        if (GUI.Button(new Rect(10, 80, 80, 50), "Delete All"))
+        {
+            SpawnObject = GameObject.Find("Parent").GetComponentsInChildren<Transform>();
+
+            for (int i = 1; i < SpawnObject.Length; i++)
+            {
+                Destroy(SpawnObject[i].gameObject);
+                nName = 0;
+            }
+        }
+    }
+}
